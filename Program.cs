34a8d6e6c2a9f865383b33f34742cc6685998c70
Alena Samus <elensawelu@gmail.com>
2023-04-14 13:11:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string s = "DCXXI";
+string currentTwoCharacters = string.Empty;
+int result = 0;
+int i = -1;
+s = s.ToUpper();
+
+
+Dictionary<String, int> numbers = new Dictionary<String, int>()
+{
+    {"I",1},
+    {"V",5},
+    {"X",10},
+    {"L",50},
+    {"C",100},
+    {"D",500},
+    {"M",1000},
+    {"IV",4},
+    {"IX",9},
+    {"XL",40},
+    {"XC",90},
+    {"CD",400},
+    {"CM",900}
+};
+
+
+    do
+    {
+        i++;
+        if (i == s.Length - 1)
+    {
+        result += numbers.TryGetValue(s[i].ToString(), out result) ? result : 0;
+    }
+        else if (i < s.Length - 1)
+        {
+            currentTwoCharacters = s[i].ToString() + s[i + 1].ToString();
+
+            if (numbers.ContainsKey(currentTwoCharacters))
+            {
+                result += numbers.TryGetValue(currentTwoCharacters, out result) ? result : 0;
+            i++;
+            continue;
+
+            }
+            else
+            {
+                result += numbers.TryGetValue(s[i].ToString(), out result) ? result : 0;
+            }
+        }
+}
+    while (i < s.Length);
+
+
+
+Console.Write(result);
+
+
+
+
